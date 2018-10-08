e746e6ef8bf52be9ab9d07bdc0c110c479fc5524
abisummers <hello@abisummers.com>
2018-10-08 13:10:04 +0200

    added button to show current time when clicked

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const secondHand = document.querySelector(".second-hand");
 const minuteHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
 
+const time = document.querySelector(".time");
+const button = document.querySelector(".buttonClick");
+const now = new Date();
+
 function setDate() {
   const now = new Date();
-
   const seconds = now.getSeconds();
   const secondsDegrees = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
@@ -19,3 +22,8 @@ function setDate() {
 }
 
 setInterval(setDate, 1000);
+
+function revealTime() {
+  button.innerHTML = "Current Time";
+  time.innerHTML = now.toLocaleTimeString();
+}
